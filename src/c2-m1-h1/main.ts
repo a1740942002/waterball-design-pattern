4217dce61cbe599b5a3fa7589543eb2308a4981c
a1740942002 <a1740942002@me.com>
2022-12-31 03:13:37 +0800

    rename folder

--- src/c2-m1-h1/main.ts
+++ src/c2-m1-h1/main.ts
@@ -2,12 +2,16 @@ import Individual from './Individual'
 import { ReverseDecorator } from './Decorator'
 import { DistanceBasedMatchingStrategy } from './Strategy'
 import MatchingSystem from './MatchingSystem'
-import mockData from './example/individuals.json'
+import mockData from './mockData/individuals.json'
 
 function main() {
   const matchingSystem = new MatchingSystem(mockData as Individual[])
 
-  const matchingResult = matchingSystem.getMatching(1, new DistanceBasedMatchingStrategy(), [new ReverseDecorator()])
+  const matchingResult = matchingSystem.getMatching(
+    1,
+    new DistanceBasedMatchingStrategy(),
+    [new ReverseDecorator()]
+  )
 
   console.log('matchingResult', matchingResult)
 }
